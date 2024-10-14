288aa927ef40826f146728344e0acaeabbff457b
Nick Saurino <nicksaurinocruz@gmail.com>
2024-10-14 14:40:21 -0500

    Using mono and flux

--- src/main/java/com/ufostyle/demo_reactor/DemoReactorApplication.java
+++ src/main/java/com/ufostyle/demo_reactor/DemoReactorApplication.java
@@ -6,7 +6,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class DemoReactorApplication implements CommandLineRunner {
@@ -20,12 +24,42 @@ public class DemoReactorApplication implements CommandLineRunner {
 				.subscribe(person -> log.info("[Reactor] Person: " + person));
 	}
 
+	//Using Mono
+	public void mono() {
+		Mono.just(new Person(1, "Jostin", "Martinez", 24))
+				.subscribe(person -> log.info(person.toString()));
+	}
+
+	//Using Flux
+	public void flux() {
+		List<Person> persons = new ArrayList<>();
+		persons.add(new Person(1, "Marcos", "Basurto", 26));
+		persons.add(new Person(2, "Valentina", "Nappi", 27));
+		persons.add(new Person(3, "Fabio", "Garcia", 28));
+
+		Flux.fromIterable(persons).subscribe(person -> log.info(person.toString()));
+	}
+
+	//Go From Flux To Mono
+	public void fluxMono() {
+		List<Person> persons = new ArrayList<>();
+		persons.add(new Person(1, "Marcos", "Basurto", 26));
+		persons.add(new Person(2, "Valentina", "Nappi", 27));
+		persons.add(new Person(3, "Fabio", "Garcia", 28));
+
+		Flux<Person> fx = Flux.fromIterable(persons);
+		fx.collectList().subscribe(listPerson -> log.info(listPerson.toString()));
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoReactorApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		reactor();
+		//reactor();
+		//mono();
+		//flux();
+		fluxMono();
 	}
 }
